b6542d92fb17b0d20dc1e10497c89bd99c9c7f45
Nickernator <nickwindt@hotmail.nl>
2018-03-16 09:25:33 +0100

    Pushing SaleDAO / SaleController AGAIN

--- src/main/java/Persistance/Dao/SaleDao.java
+++ src/main/java/Persistance/Dao/SaleDao.java
@@ -1,13 +1,49 @@
 package Persistance.Dao;
 
+import Model.Account;
 import Model.Sale;
 import Persistance.Generic.GenericController;
 import Persistance.Generic.GenericDao;
+import org.hibernate.Session;
+
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class SaleDao extends GenericDao<Sale> implements GenericController<Sale> {
 
     public SaleDao(){
         super();
+    }
+
+    public ArrayList<Sale> findByThisWeek(){
+
+        // Get current week and year
+        Calendar cal = Calendar.getInstance();
+        int week = (cal.get(Calendar.WEEK_OF_YEAR));
+        int year = cal.get(Calendar.YEAR);
+
+        ArrayList<Sale> saleList = new ArrayList<>();
+        Session session = super.getSession();
+
+        try {
+            Query query = session.createQuery("from " + Sale.class.getName() + " where (WEEK)fromDate = :week AND (YEAR)fromDate = :year ");
+            query.setParameter("week", week);
+            query.setParameter("year", year);
+
+            saleList = (ArrayList) query.getResultList();
+        } catch (Exception e) {
+            //e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        // return the found sales of this week
+        return saleList;
+
+
+
     }
 
 }
