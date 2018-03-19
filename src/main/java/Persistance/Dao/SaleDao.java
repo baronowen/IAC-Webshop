d0cc7ebeea5b3779dde6e69c154f958032880966
Nickernator <nickwindt@hotmail.nl>
2018-03-19 11:32:22 +0100

    Trying to fix login

--- src/main/java/Persistance/Dao/SaleDao.java
+++ src/main/java/Persistance/Dao/SaleDao.java
@@ -9,6 +9,7 @@ import org.hibernate.Session;
 import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class SaleDao extends GenericDao<Sale> implements GenericController<Sale> {
@@ -21,28 +22,24 @@ public class SaleDao extends GenericDao<Sale> implements GenericController<Sale>
 
         // Get current week and year
         Calendar cal = Calendar.getInstance();
-        int week = (cal.get(Calendar.WEEK_OF_YEAR));
-        int year = cal.get(Calendar.YEAR);
+        Date date = cal.getTime();
 
         ArrayList<Sale> saleList = new ArrayList<>();
         Session session = super.getSession();
 
         try {
-            Query query = session.createQuery("from " + Sale.class.getName() + " where (WEEK)fromDate = :week AND (YEAR)fromDate = :year ");
-            query.setParameter("week", week);
-            query.setParameter("year", year);
+            Query query = session.createQuery("from " + Sale.class.getName() + " where :date BETWEEN fromDate AND toDate");
+            query.setParameter("date", date);
 
             saleList = (ArrayList) query.getResultList();
         } catch (Exception e) {
-            //e.printStackTrace();
+            e.printStackTrace();
         } finally {
             session.close();
         }
 
         // return the found sales of this week
         return saleList;
-
-
 
     }
 
