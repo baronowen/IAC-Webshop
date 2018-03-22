a1c6ec7ff31cad8d25a02786050c3ddef8d379c4
YorinOuwerker96 <yorin.ouwerkerk@student.hu.nl>
2018-03-22 23:08:00 +0100

    Unit tests start
    eerste paar unit tests

---
+++ src/main/java/Tests/ProductResourceTest.java
@@ -0,0 +1,30 @@
+package Tests;
+
+import Model.Product;
+import Persistance.ProductController;
+import Webservices.ProductResource;
+import Webservices.Resource;
+import io.jsonwebtoken.lang.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductResourceTest {
+
+    @org.junit.jupiter.api.Test
+    void getAllProducts() {
+        ProductResource pTest = new ProductResource();
+        Assert.notNull(pTest.getAllProducts());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProductById() {
+        ProductResource pTest = new ProductResource();
+        ProductController p = ProductController.getInstance();
+        ArrayList<Product> products = p.findAll();
+        Assert.notNull(pTest.getProductById(products.get(0).getId()));
+    }
+
+}
